777fd083055108549dffb9992174346a333965f5
Javi Fontan <jfontan@gmail.com>
2008-04-22 02:19:48 +0200

    Templates and simple group add.
      * Solved problem with stone
      * Layout and some templates
      * Basic group add

--- manchea.rb
+++ manchea.rb
@@ -5,8 +5,21 @@ require 'ramaze'
 require 'stone'
 
 class MainController < Ramaze::Controller
+    engine :Haml
+    layout :layout
+    
     def index
-        'Benvido a manchea!'
+        "lero"
+    end
+    
+    def new_group
+        if request.post?
+            group=Group.new
+            group.name=request["name"]
+            group.description=request["description"]
+            group.public=(request["public"] ? 1 : 0)
+            group.save
+        end
     end
 end
 
@@ -14,6 +27,6 @@ end
 # Initialize stone persistence library
 Stone.start(Dir.pwd, Dir.glob(File.join(Dir.pwd,"models/*")))
 
-Ramaze.start
-
+Ramaze.start 
+#:load_engines => [:Haml, :Sass]
 
